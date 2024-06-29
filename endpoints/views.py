301d7d26382c22258a418f0b89de50aa308bfaf6
Vanessa Tejerina <vanetejerina314@gmail.com>
2024-06-29 11:50:11 -0400

    Second commit

--- endpoints/views.py
+++ endpoints/views.py
@@ -4,4 +4,5 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def index(request):
     if request.method == 'GET':
-        return JsonResponse({'message': 'Hello, world!'})
+        params = request.GET.get('params', "No se encontro el parametro 'params' en la URL")
+        return JsonResponse({'message': f'Hello, world! {params}'})
